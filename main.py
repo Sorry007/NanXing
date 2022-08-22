d2e9dfa8e5c25516df1c662afa590b5f957c4acb
Sorry_007 <991785983@qq.com>
2022-08-22 18:35:49 +0800

    更新天气信息

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ def get_today():
 def get_weather():
   api_url = 'http://apis.juhe.cn/simpleWeather/query'
   params_dict = {
-      "city": "浦东新区",  # 查询天气的城市名称，如：北京、苏州、上海
+      "city": city,  # 查询天气的城市名称，如：北京、苏州、上海
       "key": "5bf0c43ad287a8d60e506cbd8f9577cd",  # 您申请的接口API接口请求Key
   }
   params = urllib.parse.urlencode(params_dict)
@@ -127,7 +127,7 @@ def get_weather():
   direct= "4"
   power= "5"
   aqi= "6"
-  message_ = "记得爱自己💖"
+  message_ = "记得爱自己"
   try:
       req = request.Request(api_url, params.encode())
       response = request.urlopen(req)
@@ -149,6 +149,26 @@ def get_weather():
                   power = result['result']['realtime']['power']
                   #空气质量
                   aqi = result['result']['realtime']['aqi']
+                  if "晴" in info:
+                    if temperature >= 35:
+                      message_ = "今日温度过高，注意做好防晒呀🌞"
+                    else:
+                      message_ = "今日温度适宜，记得多晒太阳，长高高~😊"
+
+                  if "云" in info:
+                    message_ = "今日温度适宜，记得多晒太阳，长高高~😊" 
+
+                  if "雨" in info:
+                    message_ = "今天可能会下雨🌧，记得带伞🌂" 
+
+                  if "雪" in info:
+                    message_ = "今天可能会下雪🌨，记得做好防护呀~☔" 
+
+                  if "阴" in info:
+                    message_ = "今天可能见不到太阳啦，但也不能没有阳光呀🌞" 
+
+                  if "雾" in info:
+                    message_ = "今天可见度会降低，小心别迷路呀~😜"
                   print("温度：%s\n湿度：%s\n天气：%s\n天气标识：%s\n风向：%s\n风力：%s\n空气质量：%s" % (
                       temperature, humidity, info, direct, power, aqi))
                   return temperature,humidity,info,direct,power,aqi,message_
