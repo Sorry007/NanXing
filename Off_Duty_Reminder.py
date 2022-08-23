96a77dca265fe7d0d03bda3b161609e2e8998544
Sorry_007 <991785983@qq.com>
2022-08-23 10:01:13 +0800

    修改天气接口

--- Off_Duty_Reminder.py
+++ Off_Duty_Reminder.py
@@ -6,6 +6,8 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import os
 import random
 import requests
+import http.client
+import json
 
 today = datetime.now()
 
@@ -38,6 +40,52 @@ def get_words():
     return get_words()
   return words.json()['data']['text']
 
+def get_weather():
+    conn = http.client.HTTPConnection("eolink.o.apispace.com")
+    payload = ""
+    headers = {
+        "X-APISpace-Token":"f59cbztv2k9jfcgn84ilxv6kymglm5s7",
+        "Authorization-Type":"apikey"
+    }
+    conn.request("GET","/456456/weather/v001/now?areacode=101020600", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    result = json.loads(data)
+    #体感温度
+    temperature = result['result']['realtime']['feels_like']
+    #湿度
+    humidity = result['result']['realtime']['rh']
+    #天气
+    info = result['result']['realtime']['text']
+    #风向
+    direct = result['result']['realtime']['wind_dir']
+    #风力
+    power = result['result']['realtime']['wind_class']
+    if "晴" in info:
+      if temperature >= 35:
+        message_ = "今日温度过高，注意做好防晒~"
+      else:
+        message_ = "今日温度适宜，记得多晒太阳，长高高~"
+
+    if "云" in info:
+      message_ = "今日温度适宜，记得多晒太阳，长高高~" 
+
+    if "雨" in info:
+      message_ = "今天可能会下雨🌧，记得带伞~" 
+
+    if "雪" in info:
+      message_ = "今天可能会下雪🌨，记得做好防护~" 
+
+    if "阴" in info:
+      message_ = "今天可能见不到太阳啦~" 
+
+    if "雾" in info:
+      message_ = "今天可见度会降低，小心别迷路~"
+      
+    return temperature,humidity,info,direct,power,message_
+
+
+
 # 获取下班提醒
 def get_message():
   message_ = "马上就要下班啦，记得打卡，带好自己的东西~"
@@ -50,10 +98,16 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 message = get_message()
+wm_temperature, wm_humidity, wm_info, wm_direct, wm_power, wm_message= get_weather()
 data = {
   "dateTime":{"value":get_today()+" "+get_weekdays(),"color":get_random_color()},
-  "words":{"value":get_words(),"color":get_random_color()},
-  "message":{"value":""+ message ,"color":get_random_color()}
+  "message":{"value":""+ message ,"color":get_random_color()},
+  "weather":{"value":""+wm_info,"color":get_random_color()},
+  "temperature":{"value":""+wm_temperature,"color":get_random_color()},
+  "humidity":{"value":""+wm_humidity,"color":get_random_color()},
+  "direct":{"value":""+wm_direct,"color":get_random_color()},
+  "power":{"value":""+wm_power,"color":get_random_color()},
+  "wm_message":{"value":""+wm_message,"color":get_random_color()}
   }
   
 res_boy = wm.send_template(boy_friend_id, template_id, data)
